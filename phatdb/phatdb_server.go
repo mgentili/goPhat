6f72229fd5d0d8aee09b2b42c86e3933ecee3843
Stephen Merity <smerity@smerity.com>
2014-03-04 13:20:06 -0500

    phatdb: Server commands implemented but more testing needed

--- phatdb/phatdb_server.go
+++ phatdb/phatdb_server.go
@@ -1,6 +1,7 @@
 package phatdb
 
 import (
+	"errors"
 	"log"
 )
 
@@ -25,9 +26,26 @@ func DatabaseServer(input chan DBCommand) {
 		req := <-input
 		log.Printf("Received request: %s", req.Command)
 		switch req.Command {
+		case "CHILDREN":
+			kids, err := getChildren(root, req.Path)
+			req.Done <- &DBResponse{kids, err}
 		case "CREATE":
 			n, err := createNode(root, req.Path, req.Value)
 			req.Done <- &DBResponse{n, err}
+		case "DELETE":
+			n, err := deleteNode(root, req.Path)
+			req.Done <- &DBResponse{n, err}
+		case "EXISTS":
+			n, err := existsNode(root, req.Path)
+			req.Done <- &DBResponse{n, err}
+		case "GET":
+			n, err := getNode(root, req.Path)
+			req.Done <- &DBResponse{n, err}
+		case "SET":
+			n, err := setNode(root, req.Path, req.Value)
+			req.Done <- &DBResponse{n, err}
+		default:
+			req.Done <- &DBResponse{nil, errors.New("Unknown command")}
 		}
 	}
 }
--- phatdb/phatdb_server_test.go
+++ phatdb/phatdb_server_test.go
@@ -9,18 +9,34 @@ func TestDatabaseServer(t *testing.T) {
 	input := make(chan DBCommand)
 	go DatabaseServer(input)
 	//
+	// A bad command should fail
+	badCmd := DBCommand{"HAMMERTIME", "", "", make(chan *DBResponse)}
+	input <- badCmd
+	// TODO: Ensure it's the expected error
+	if resp := <-badCmd.Done; resp.Reply != nil || resp.Error == nil {
+		t.Errorf("A bad command returned non-error response")
+	}
 	// Create should succeed
-	setCmd := DBCommand{"CREATE", "/dev/null", "empty", make(chan *DBResponse)}
-	input <- setCmd
-	resp := <-setCmd.Done
-	if (resp.Reply.(*FileNode)).Data != "empty" || resp.Error != nil {
+	createCmd := DBCommand{"CREATE", "/dev/null", "empty", make(chan *DBResponse)}
+	input <- createCmd
+	if resp := <-createCmd.Done; (resp.Reply.(*FileNode)).Data != "empty" || resp.Error != nil {
 		t.Errorf("CREATE that should work has failed")
 	}
 	// Try to create a file that already exists
-	input <- setCmd
-	resp = <-setCmd.Done
-	if resp.Reply != nil || resp.Error != os.ErrExist {
+	input <- createCmd
+	if resp := <-createCmd.Done; resp.Reply.(*FileNode) != nil || !os.IsExist(resp.Error) {
 		t.Errorf("CREATE has succeeded even though file already exists")
 	}
 	//
+	getCmd := DBCommand{"GET", "/dev/null", "", make(chan *DBResponse)}
+	input <- getCmd
+	if resp := <-getCmd.Done; resp.Reply.(*FileNode).Data != "empty" || resp.Reply.(*FileNode).Version != 1 || resp.Error != nil {
+		t.Errorf("GET fails")
+	}
+	//
+	setCmd := DBCommand{"SET", "/dev/null", "nullify", make(chan *DBResponse)}
+	input <- setCmd
+	if resp := <-setCmd.Done; resp.Reply.(*FileNode).Data != "nullify" || resp.Reply.(*FileNode).Version != 2 || resp.Error != nil {
+		t.Errorf("SET fails")
+	}
 }
