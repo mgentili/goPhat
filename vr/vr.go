f5ade62082ae0eb9f22dbc7ea091ab065a95f535
Nathaniel Herman <nherman@college.harvard.edu>
2014-03-09 12:48:20 -0400

    change vr.go code to not use any global objects--should make testing much easier

--- vr/vr.go
+++ vr/vr.go
@@ -16,11 +16,6 @@ const (
 	MAX_RENEWAL = LEASE / 2
 )
 
-var clients [NREPLICAS + 1]*rpc.Client
-
-var rstate ReplicaState
-var mstate MasterState
-
 // a replica's possible states
 const (
 	Normal = iota
@@ -30,7 +25,16 @@ const (
 
 type Command string
 
-var phatlog []string
+type Replica struct{
+    Rstate ReplicaState
+    Mstate MasterState
+	// list of replica addresses, in sorted order
+	Config []string
+    Clients [NREPLICAS + 1]*rpc.Client
+    //TEMP
+    Phatlog []string
+}
+
 
 type ReplicaState struct {
 	View           uint
@@ -41,8 +45,6 @@ type ReplicaState struct {
 	NormalView     uint
 	ViewChangeMsgs uint
 	Timer          *time.Timer
-	// list of replica addresses, in sorted order
-	Config []string
 }
 
 type MasterState struct {
@@ -91,6 +93,7 @@ type CommitArgs struct {
 	CommitNumber uint
 }
 
+// Go doesn't have assertions...
 func assert(b bool) {
 	if !b {
 		log.Fatal("assertion failed")
@@ -101,293 +104,273 @@ func wrongView() error {
 	return errors.New("view numbers don't match")
 }
 
-func addLog(command interface{}) {
-	phatlog = append(phatlog, command.(string))
-	log.Printf("replica %d logging\n", rstate.ReplicaNumber)
+func (r *Replica) addLog(command interface{}) {
+	r.Phatlog = append(r.Phatlog, command.(string))
+    r.Debug("adding command to log")
 	//    phatlog.add(command)
 }
 
-func (r *ReplicaState) Debug(format string, args ...interface{}) {
-	str := fmt.Sprintf("Replica %d: %s", r.ReplicaNumber, format)
+func (r *Replica) Debug(format string, args ...interface{}) {
+	str := fmt.Sprintf("Replica %d: %s", r.Rstate.ReplicaNumber, format)
 	log.Printf(str, args...)
 }
 
-func doCommit(cn uint) {
-	if cn <= rstate.CommitNumber {
-		log.Printf("Ignoring commit %d, already commited up to %d", cn, rstate.CommitNumber)
+func (r *Replica) doCommit(cn uint) {
+	if cn <= r.Rstate.CommitNumber {
+		r.Debug("Ignoring commit %d, already commited up to %d", cn, r.Rstate.CommitNumber)
 		return
-	} else if cn > rstate.OpNumber {
-		log.Printf("replica %d needs to do state transfer. only at op %d in log but got commit for %d\n", rstate.ReplicaNumber, rstate.OpNumber, cn)
+	} else if cn > r.Rstate.OpNumber {
+		r.Debug("need to do state transfer. only at op %d in log but got commit for %d\n", r.Rstate.OpNumber, cn)
 		return
-	} else if cn > rstate.CommitNumber+1 {
-		log.Printf("replica %d needs to do extra commits", rstate.ReplicaNumber)
+	} else if cn > r.Rstate.CommitNumber+1 {
+		r.Debug("need to do extra commits")
 		// we're behind (but have all the log entries, so don't need to state
 		// transfer), so catch up by committing up to the current commit
-		for i := rstate.CommitNumber + 1; i < cn; i++ {
-			doCommit(i)
+		for i := r.Rstate.CommitNumber + 1; i < cn; i++ {
+			r.doCommit(i)
 		}
 	}
-	//    rstate.Commit()
-	rstate.CommitNumber++
+	//db.Commit()
+	r.Rstate.CommitNumber++
 }
 
 // RPCs
-type Replica struct{}
+func (r *Replica) Prepare(args *PrepareArgs, reply *PrepareReply) error {
+	r.Debug("Got prepare %d\n", args.OpNumber)
 
-func (t *Replica) Prepare(args *PrepareArgs, reply *PrepareReply) error {
-	log.Printf("Replica %d got prepare %d\n", rstate.ReplicaNumber, args.OpNumber)
-
-	if args.View != rstate.View {
+	if args.View != r.Rstate.View {
 		return wrongView()
 	}
 
-	rstate.ExtendLease() // do we extend lease in non-normal mode??
+	r.Rstate.ExtendLease() // do we extend lease in non-normal mode??
 
-	if rstate.Status != Normal {
+	if r.Rstate.Status != Normal {
 		// TODO: ideally we should just not respond or something in this case
 		return errors.New("not in normal mode")
 	}
 
-	if args.OpNumber != rstate.OpNumber+1 {
+	if args.OpNumber != r.Rstate.OpNumber+1 {
 		// TODO: we should probably sleep or something til this is true??
-		return fmt.Errorf("op numbers out of sync: got %d expected %d", args.OpNumber, rstate.OpNumber+1)
+		return fmt.Errorf("op numbers out of sync: got %d expected %d", args.OpNumber, r.Rstate.OpNumber+1)
 	}
 
-	rstate.OpNumber++
-	addLog(args.Command)
+	r.Rstate.OpNumber++
+	r.addLog(args.Command)
 
-	reply.View = rstate.View
-	reply.OpNumber = rstate.OpNumber
-	reply.ReplicaNumber = rstate.ReplicaNumber
+	reply.View = r.Rstate.View
+	reply.OpNumber = r.Rstate.OpNumber
+	reply.ReplicaNumber = r.Rstate.ReplicaNumber
 
 	return nil
 }
 
-func (t *Replica) Commit(args *CommitArgs, reply *uint) error {
-	if args.View != rstate.View {
+func (r *Replica) Commit(args *CommitArgs, reply *uint) error {
+	if args.View != r.Rstate.View {
 		return wrongView()
 	}
 
-	rstate.ExtendLease()
+	r.Rstate.ExtendLease()
 
-	*reply = rstate.ReplicaNumber
+	*reply = r.Rstate.ReplicaNumber
 
-	doCommit(args.CommitNumber)
+	r.doCommit(args.CommitNumber)
 
 	return nil
 }
 
-func (rstate *ReplicaState) IsMaster() bool {
-	return rstate.View%(NREPLICAS+1) == rstate.ReplicaNumber
+func (r *Replica) IsMaster() bool {
+	return r.Rstate.View%(NREPLICAS+1) == r.Rstate.ReplicaNumber
 }
 
 func (mstate *MasterState) Reset() {
 	mstate.A = 0
 	mstate.Replies = 0
+    mstate.Heartbeats = 0
+    mstate.HeartbeatReplies = 0
 }
 
-func (m *MasterState) ExtendNeedsRenewal() {
-	m.Timer.Reset(MAX_RENEWAL)
+func (mstate *MasterState) ExtendNeedsRenewal() {
+	mstate.Timer.Reset(MAX_RENEWAL)
 }
 
-func (m *MasterState) Heartbeat(replica uint) {
-	if ((1 << replica) & mstate.HeartbeatReplies) != 0 {
+func (r *Replica) Heartbeat(replica uint) {
+    assert(r.IsMaster())
+
+	if ((1 << replica) & r.Mstate.HeartbeatReplies) != 0 {
 		return
 	}
 
-	m.HeartbeatReplies |= 1 << replica
-	m.Heartbeats++
+	r.Mstate.HeartbeatReplies |= 1 << replica
+	r.Mstate.Heartbeats++
 
 	// we've gotten a majority to renew our lease!
-	if m.Heartbeats == F {
-		m.Heartbeats = 0
-		m.HeartbeatReplies = 0
+	if r.Mstate.Heartbeats == F {
+		r.Mstate.Heartbeats = 0
+		r.Mstate.HeartbeatReplies = 0
 		// don't need to try renewing for a while
-		m.ExtendNeedsRenewal()
+		r.Mstate.ExtendNeedsRenewal()
 		// update our own lease
-		rstate.ExtendLease()
+		r.Rstate.ExtendLease()
 	}
 }
 
-func (r *ReplicaState) ExtendLease() {
-	r.Timer.Reset(LEASE)
+func (rstate *ReplicaState) ExtendLease() {
+	rstate.Timer.Reset(LEASE)
 }
 
-func ReplicaTimeout() {
-	if rstate.IsMaster() {
-		log.Printf("we couldn't stay master :(\n")
+func (r *Replica) ReplicaTimeout() {
+	if r.IsMaster() {
+		r.Debug("we couldn't stay master :(\n")
 		// can't handle read requests anymore
 	}
-	log.Printf("replica %d timed out, trying view change", rstate.ReplicaNumber)
-	PrepareViewChange()
+	r.Debug("Timed out, trying view change")
+//	r.PrepareViewChange()
 	// start counting again so we timeout if the new replica can't become master
-	rstate.ExtendLease()
+	r.Rstate.ExtendLease()
 }
 
-func MasterNeedsRenewal() {
-	// TODO: master's been inactive for MAX_RENEWAL time, so we need to
+func (r *Replica) MasterNeedsRenewal() {
+	// TODO: we've been inactive for MAX_RENEWAL time, so we need to
 	// send explicit commits to renew lease
 }
 
 func RunAsReplica(i uint, config []string) {
-	rstate.ReplicaNumber = i
-	rstate.Config = config
+    r := new(Replica)
+	r.Rstate.ReplicaNumber = i
+	r.Config = config
 
-	ln := ReplicaInit()
+	ln := r.ReplicaInit()
 
-	go ReplicaRun(ln)
-	if rstate.IsMaster() {
-		MasterInit()
+	go r.ReplicaRun(ln)
+	if r.IsMaster() {
+		r.MasterInit()
 		for {
-			RunVR("foo")
-			RunVR("bar")
+			r.RunVR("foo")
+			r.RunVR("bar")
 			time.Sleep(10 * time.Millisecond)
 		}
 	}
 }
 
-// HAX: need to setup so each replica has a connection setup with each other replica
-// but have to wait til all replicas are up to do this. so for now just assume that's
-// true after receiving the first rpc
-// this really needs to be more robust, e.g. handling reconnections
-var hasInterconnect bool = false
+func (r *Replica) MasterInit() {
+    assert(r.IsMaster())
 
-// sets up connections with all other replicas
-func InterconnectionInit() {
-	return
-	if hasInterconnect {
-		return
-	}
-	hasInterconnect = true
-	j := 0
-	var i uint = 0
-	for ; i < NREPLICAS+1; i++ {
-		if i == rstate.ReplicaNumber {
-			continue
-		}
-		c, err := rpc.Dial("tcp", rstate.Config[i])
-		if err != nil {
-			// couldn't connect to some replica, try to just keep going
-			// (could probably handle this better...)
-			fmt.Println(err)
-		}
-		clients[j] = c
-		j++
-	}
+    r.Mstate.Reset()
+	r.Mstate.Timer = time.AfterFunc(MAX_RENEWAL, r.MasterNeedsRenewal)
 }
 
-func MasterInit() {
-	InterconnectionInit()
-	mstate.Timer = time.AfterFunc(MAX_RENEWAL, MasterNeedsRenewal)
-}
-
-func ReplicaInit() net.Listener {
-	rpc.Register(new(Replica))
-	ln, err := net.Listen("tcp", rstate.Config[rstate.ReplicaNumber])
+func (r *Replica) ReplicaInit() net.Listener {
+	ln, err := net.Listen("tcp", r.Config[r.Rstate.ReplicaNumber])
 	if err != nil {
 		fmt.Println(err)
 		return nil
 	}
-	rstate.Timer = time.AfterFunc(LEASE, ReplicaTimeout)
+	r.Rstate.Timer = time.AfterFunc(LEASE, r.ReplicaTimeout)
 	return ln
 }
 
 // TODO: might not need to do this, e.g. if we handle client and server rpcs all on the same port
-func ReplicaRun(ln net.Listener) {
+func (r *Replica) ReplicaRun(ln net.Listener) {
+    newServer := rpc.NewServer()
+
+    newServer.Register(r)
+
 	for {
-		c, err := ln.Accept()
+/*		c, err := ln.Accept()
 		if err != nil {
 			continue
 		}
-		InterconnectionInit()
 
 		rpc.ServeConn(c)
+*/
+        newServer.Accept(ln)
+        r.Debug("finished an accept?")
 	}
 }
 
-func RunVR(command interface{}) {
-	assert(rstate.IsMaster() /*&& holdLease()*/)
+func (r *Replica) RunVR(command interface{}) {
+	assert(r.IsMaster() /*&& holdLease()*/)
 
 	// FIXME: right now we enforce that the last operation has been committed before starting a new one
-	assert(rstate.OpNumber == rstate.CommitNumber)
+	assert(r.Rstate.OpNumber == r.Rstate.CommitNumber)
 
-	mstate.Reset()
+	r.Mstate.Reset()
 
-	rstate.OpNumber++
-	addLog(command)
+	r.Rstate.OpNumber++
+	r.addLog(command)
 
-	args := PrepareArgs{rstate.View, command, rstate.OpNumber, rstate.CommitNumber}
+	args := PrepareArgs{r.Rstate.View, command, r.Rstate.OpNumber, r.Rstate.CommitNumber}
 	replyConstructor := func() interface{} { return new(PrepareReply) }
-	sendAndRecv(NREPLICAS, "Replica.Prepare", args, replyConstructor, func(reply interface{}) bool {
-		return handlePrepareOK(reply.(*PrepareReply))
+	r.sendAndRecv(NREPLICAS, "Replica.Prepare", args, replyConstructor, func(reply interface{}) bool {
+		return r.handlePrepareOK(reply.(*PrepareReply))
 	})
 }
 
-func handlePrepareOK(reply *PrepareReply) bool {
-	log.Printf("got response: %+v\n", reply)
-	if reply.View != rstate.View {
+func (r *Replica) handlePrepareOK(reply *PrepareReply) bool {
+	r.Debug("got response: %+v\n", reply)
+	if reply.View != r.Rstate.View {
 		return false
 	}
 
-	mstate.Heartbeat(reply.ReplicaNumber)
+	r.Heartbeat(reply.ReplicaNumber)
 
-	if reply.OpNumber != rstate.OpNumber {
+	if reply.OpNumber != r.Rstate.OpNumber {
 		return false
 	}
 
-	if ((1 << reply.ReplicaNumber) & mstate.Replies) != 0 {
+	if ((1 << reply.ReplicaNumber) & r.Mstate.Replies) != 0 {
 		return false
 	}
 
-	log.Printf("got suitable response\n")
+	r.Debug("got suitable response\n")
 
-	mstate.Replies |= 1 << reply.ReplicaNumber
-	mstate.A++
+	r.Mstate.Replies |= 1 << reply.ReplicaNumber
+	r.Mstate.A++
 
-	log.Printf("new master state: %v\n", mstate)
+	r.Debug("new master state: %v\n", r.Mstate)
 
 	// we've implicitly gotten a response from ourself already
-	if mstate.A != F {
-		return mstate.A >= F
+	if r.Mstate.A != F {
+		return r.Mstate.A >= F
 	}
 
 	// we've now gotten a majority
-	doCommit(rstate.CommitNumber + 1)
+	r.doCommit(r.Rstate.CommitNumber + 1)
 
-	args := CommitArgs{rstate.View, rstate.CommitNumber}
+	args := CommitArgs{r.Rstate.View, r.Rstate.CommitNumber}
 	// TODO: technically only need to do this when we don't get another request from the client for a while
-	go sendAndRecv(NREPLICAS, "Replica.Commit", args,
+	go r.sendAndRecv(NREPLICAS, "Replica.Commit", args,
 		func() interface{} { return new(uint) },
-		func(r interface{}) bool { mstate.Heartbeat(*(r.(*uint))); return false })
+		func(reply interface{}) bool { r.Heartbeat(*(reply.(*uint))); return false })
 
 	return true
 }
 
-func SendSync(repNum uint, msg string, args interface{}, reply interface{}) error {
-	if clients[repNum] == nil {
-		err := ClientConnect(repNum)
+func (r *Replica) SendSync(repNum uint, msg string, args interface{}, reply interface{}) error {
+	if r.Clients[repNum] == nil {
+		err := r.ClientConnect(repNum)
 		if err != nil {
 			return err
 		}
 	}
-	err := clients[repNum].Call(msg, args, reply)
+	err := r.Clients[repNum].Call(msg, args, reply)
 	return err
 }
 
-func ClientConnect(repNum uint) error {
-	assert(repNum != rstate.ReplicaNumber)
-	c, err := rpc.Dial("tcp", rstate.Config[repNum])
+func (r *Replica) ClientConnect(repNum uint) error {
+	assert(repNum != r.Rstate.ReplicaNumber)
+	c, err := rpc.Dial("tcp", r.Config[repNum])
 
 	if err != nil {
-		rstate.Debug("error trying to connect to replica %d: %v", repNum, err)
+		r.Debug("error trying to connect to replica %d: %v", repNum, err)
 	} else {
-		clients[repNum] = c
+		r.Clients[repNum] = c
 	}
 
 	return err
 }
 
-func sendAndRecv(N int, msg string, args interface{}, newReply func() interface{}, handler func(reply interface{}) bool) {
+func (r *Replica) sendAndRecv(N int, msg string, args interface{}, newReply func() interface{}, handler func(reply interface{}) bool) {
 
 	type ClientCall struct {
 		call *rpc.Call
@@ -398,13 +381,13 @@ func sendAndRecv(N int, msg string, args interface{}, newReply func() interface{
 	callChan := make(chan ClientCall)
 
 	sendOne := func(repNum uint) {
-		if clients[repNum] == nil {
-			err := ClientConnect(repNum)
+		if r.Clients[repNum] == nil {
+			err := r.ClientConnect(repNum)
 			if err != nil {
 				return
 			}
 		}
-		client := clients[repNum]
+		client := r.Clients[repNum]
 		call := client.Go(msg, args, newReply(), nil)
 		go func(c ClientCall) {
 			// wait for this call to complete
@@ -415,12 +398,12 @@ func sendAndRecv(N int, msg string, args interface{}, newReply func() interface{
 	}
 
 	// send requests to the replicas
-	i := 0
-	for j := uint(0); i < N && j < NREPLICAS+1; j++ {
-		if j == rstate.ReplicaNumber {
+	i := 0 // == requests sent
+	for repNum := uint(0); i < N && repNum < NREPLICAS+1; repNum++ {
+		if repNum == r.Rstate.ReplicaNumber {
 			continue
 		}
-		sendOne(j)
+		sendOne(repNum)
 		i++
 	}
 
@@ -434,11 +417,12 @@ func sendAndRecv(N int, msg string, args interface{}, newReply func() interface{
 			call := clientCall.call
 			if call.Error != nil {
 				// for now just resend failed messages indefinitely
-				log.Printf("sendAndRecv message error: %v, %T", call.Error)
+				r.Debug("sendAndRecv message error: %v", call.Error)
 				if call.Error == rpc.ErrShutdown {
-					err := ClientConnect(clientCall.repNum)
+					err := r.ClientConnect(clientCall.repNum)
 					if err != nil {
 						// for now, at least, we won't retry a second time if the connection is completely shutdown
+                        i++
 						continue
 					}
 				}
