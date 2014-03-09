c22e07ef62f0bc21e774a5c59ade47f45117550e
Nathaniel Herman <nherman@college.harvard.edu>
2014-03-09 13:33:23 -0400

    add option to run vr_exec test all as one process to illustrate how to do so

--- vr/vr_exec/vr_exec.go
+++ vr/vr_exec/vr_exec.go
@@ -5,14 +5,23 @@ import (
     "goPhat/vr"
 )
 
+var config = []string{"127.0.0.1:9000", "127.0.0.1:9001", "127.0.0.1:9002"}
+
+const N = 3
+
+
 func main() {
+    oneProcP := flag.Bool("1", false, "Run VR in 1 process")
     indP := flag.Uint("r", 0, "replica num")
     flag.Parse()
 
-    ind := *indP
-
-    var config = []string{"127.0.0.1:9000", "127.0.0.1:9001", "127.0.0.1:9002"}
-
-    vr.RunAsReplica(ind, config)
+    if *oneProcP {
+        for ind := N-1; ind >= 0; ind-- {
+            go vr.RunAsReplica(uint(ind), config)
+        }
+    } else {
+        ind := *indP
+        vr.RunAsReplica(ind, config)
+    }
     <-make(chan int)
 }
