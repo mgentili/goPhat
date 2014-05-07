80650038aa2c84a196ef6c7e956f68077784ace0
Stephen Merity <smerity@smerity.com>
2014-05-07 05:25:46 -0400

    Naive first test of the system
    + 10k sequential push + pop ops take about 15.1 secs on my laptop
    + Can't co-exist with the existing test as servers can't be shut down
    + TODO: Make windowed requests from multiple clients

--- worker/worker_test.go
+++ worker/worker_test.go
@@ -15,6 +15,7 @@ const BASE = 9000
 var replica_config = []string{"127.0.0.1:9000", "127.0.0.1:9001", "127.0.0.1:9002"}
 var client_config = []string{"127.0.0.1:6000", "127.0.0.1:6001", "127.0.0.1:6002"}
 
+/*
 func TestClientConnection(t *testing.T) {
 	for i := 0; i < 3; i = i + 1 {
 		newReplica := vr.RunAsReplica(uint(i), replica_config)
@@ -51,4 +52,37 @@ func TestClientConnection(t *testing.T) {
 	if err == nil {
 		t.Errorf(fmt.Sprintf("Expected error from pop, got %s"), err)
 	}
+}
+*/
+
+func Test10k(b *testing.T) {
+	for i := 0; i < 3; i = i + 1 {
+		newReplica := vr.RunAsReplica(uint(i), replica_config)
+		queueRPC.StartServer(client_config[i], newReplica)
+	}
+	time.Sleep(time.Second)
+	cli, err := NewWorker(client_config, 1, "w1")
+	if err != nil {
+		b.Errorf(err.Error())
+	}
+	//
+	start := time.Now()
+	for n := 0; n < 10000; n++ {
+		testString := fmt.Sprintf("hello-%d", n)
+		err = cli.Push(testString)
+		if err != nil {
+			b.Errorf(err.Error())
+		}
+		//
+		res, err := cli.Pop()
+		if err != nil {
+			b.Errorf(err.Error())
+		}
+		msg := res.Reply.(phatqueue.QMessage).Value.(string)
+		if testString != msg {
+			b.Errorf("Expected %s but received %s", testString, msg)
+		}
+	}
+	elapsed := time.Since(start)
+	log.Printf("Test10k took %s", elapsed)
 }
