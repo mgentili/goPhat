17cf97c0b0b8f58757cfc9e42c5fbb781e8be93d
BradMcDanel <mcdabw6@gmail.com>
2014-05-11 15:34:22 -0400

    changed phatqueue to use slices instead of lists

--- phatqueue/queue.go
+++ phatqueue/queue.go
@@ -1,81 +1,67 @@
 package phatqueue
 
-import (
-	"bytes"
-	"container/list"
-	"encoding/gob"
-	"strconv"
-)
+import "strconv"
+import "bytes"
+import "encoding/gob"
 
 type QMessage struct {
 	MessageID string
 	Value     interface{}
 }
 
+type LogEntry struct {
+    Message QMessage
+    Command string
+}
+
 type MessageQueue struct {
-	queue      list.List
-	inProgress map[string]QMessage
-	id         int
+	Queue           []QMessage
+	InProgress      map[string]QMessage
+	Id              int
 }
 
 func (mq *MessageQueue) Init() {
-	mq.inProgress = make(map[string]QMessage)
+	mq.InProgress = make(map[string]QMessage)
 }
 
 func (mq *MessageQueue) NextID() int {
-	mq.id += 1
-	return mq.id
+	mq.Id += 1
+	return mq.Id
 }
 
 func (mq *MessageQueue) Push(v interface{}) {
 	qm := QMessage{strconv.Itoa(mq.NextID()), v}
-	mq.queue.PushBack(qm)
+	mq.Queue = append(mq.Queue, qm)
 }
 
 func (mq *MessageQueue) Pop() *QMessage {
 	if mq.Len() == 0 {
 		return nil
 	}
-	e := mq.queue.Front()
-	qmesg := e.Value.(QMessage)
-	mq.queue.Remove(e)
-	// TODO: When we actually care about "in progress" messages
-	// Until then, this is equivalent to a memory leak
-	//mq.inProgress[qmesg.MessageID] = qmesg
-	return &qmesg
+    var qm QMessage
+    qm, mq.Queue = mq.Queue[len(mq.Queue)-1], mq.Queue[:len(mq.Queue)-1]
+	return &qm
 }
 
-func (mq *MessageQueue) Done(mid string) {
-	// TODO: Ensure it exists and return an error otherwise
-	delete(mq.inProgress, mid)
+func (mq *MessageQueue) Done(mId string) {
+	delete(mq.InProgress, mId)
 }
 
 func (mq *MessageQueue) Len() int {
-	return mq.queue.Len()
+	return len(mq.Queue)
 }
 
 func (mq *MessageQueue) LenInProgress() int {
-	return len(mq.inProgress)
+	return len(mq.InProgress)
 }
 
-func (mq *MessageQueue) Copy() (newmq *MessageQueue) {
-	newmq = new(MessageQueue)
-	newmq.Init()
-	// newmq.queue is initially empty so this effectively copies mq.queue to it
-	newmq.queue.PushBackList(&mq.queue)
-	for k, v := range mq.inProgress {
-		newmq.inProgress[k] = v
-	}
-	newmq.id = mq.id
-	return
-}
-
+//convert the queue to a byte slice
 func (mq *MessageQueue) Bytes() ([]byte, error) {
 	var queueState bytes.Buffer
 	enc := gob.NewEncoder(&queueState)
-	err := enc.Encode(mq.queue)
-	err = enc.Encode(mq.inProgress)
-	err = enc.Encode(mq.id)
+	err := enc.Encode(mq.Queue)
+	err = enc.Encode(mq.InProgress)
+	err = enc.Encode(mq.Id)
 	if err != nil {
 		return nil, err
 	}
